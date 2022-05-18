5e33601fc89d9a4c96208473883736118c2464fc
Jonathan Oppenheimer <147infiniti@gmail.com>
2022-05-17 20:13:09 -0400

    Time: 254 ms (17.69%), Space: 87.4 MB (6.39%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.c
+++ 234-palindrome-linked-list/234-palindrome-linked-list.c
@@ -15,7 +15,6 @@ struct double_list {
 
 
 bool isPalindrome(struct ListNode* head){
-
     struct ListNode* head_head = head; 
     struct double_list* doubly = malloc(sizeof(struct double_list));
     struct double_list* doubly_head = doubly; 
@@ -41,48 +40,5 @@ bool isPalindrome(struct ListNode* head){
         doubly = doubly->prev; 
     }
     
-    return palindrome; 
-    
-    
-//     int length = 0; 
-//     struct ListNode * maintain_head = head; 
-//     while(head != NULL) {
-//         head = head->next;
-//         length++;
-//     }
-//     head = maintain_head; 
-    
-//     struct ListNode* reversed = malloc(sizeof(struct ListNode));
-//     assert(reversed != NULL);
-//     reversed->val = 0; 
-//     reversed->next = NULL;
-//     struct ListNode * maintain_head_reversed = reversed; 
-    
-//     int count_down = length - 1; 
-//     while(count_down >= 0) {
-//         for(int i = count_down; i > 0; i--) {
-//             head=head->next; 
-//         } 
-//         reversed->val = head->val; 
-//         printf("%d\n", reversed->val); 
-//         struct ListNode* next = malloc(sizeof(struct ListNode));
-//         assert(next != NULL);
-//         next->val = 0; 
-//         next->next = NULL;
-//         reversed->next = next; 
-//         reversed = reversed->next; 
-//         head = maintain_head; 
-//         count_down--; 
-//     }
-//     reversed = maintain_head_reversed; 
-   
-//     bool all_matching = true; 
-//     for(int i=0; i<length; i++) {
-//         if(head->val != reversed->val) {
-//             all_matching = false;
-//         }
-//         head = head->next; 
-//         reversed = reversed->next;
-//     }
-//     return all_matching; 
+    return palindrome;
 }
